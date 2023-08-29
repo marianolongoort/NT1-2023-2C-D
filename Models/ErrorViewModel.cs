3b1a51224079be6897462df4f101b06745334810
marianolongoort <mariano.longo@ort.edu.ar>
2023-08-29 19:18:17 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NT1_2023_2C_D.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
