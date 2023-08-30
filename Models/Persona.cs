6e11058a05a2da4ae9a8597687e56a26f5ddb420
Mariano Longo <mariano.longo@ort.edu.ar>
2023-08-29 21:52:58 -0300

    Agrego las propiedades en la entidad Persona

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -2,5 +2,9 @@
 {
     public class Persona
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; }
+        public int Dni { get; set; }
     }
 }
